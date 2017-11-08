123f532f24d0cfb5e885c1bcbc434dd47ca0102d
Victor Lomakin <support62@office.shtorm.com>
2017-11-09 00:41:09 +0200

    Remove debug files.

--- mysite/testapp_rest/urls.py
+++
@@ -1,8 +0,0 @@
-from collections import OrderedDict
-from django.conf.urls import url
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.reverse import reverse
-
-from . import views
