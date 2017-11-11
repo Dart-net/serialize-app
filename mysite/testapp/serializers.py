3bbef2b2f237bf85be34ce4b91203849a02068b0
Juraj Vanco <Juraj.Vanco@my1.eu>
2017-11-11 22:47:02 +0000

    Serialize without base class

--- mysite/testapp/serializers.py
+++ mysite/testapp/serializers.py
@@ -13,27 +13,34 @@ class ShoppingItemNormal2Serializer(serializers.ModelSerializer):
 
 class CustomSerializer(serializers.Serializer):
     @classmethod
-    def get_serializer(cls, model):
+    def _get_serializer(cls, model):
         if model == ShoppingItemNormal1:
             return ShoppingItemNormal1Serializer
         elif model == ShoppingItemNormal2:
             return ShoppingItemNormal2Serializer
 
     def to_representation(self, instance):
-        serializer = self.get_serializer(instance.__class__)
+        serializer = self._get_serializer(instance.__class__)
         return serializer(instance, context=self.context).data
 
-class ShoppingItemBaseSerializer(serializers.ModelSerializer):
-    item_type = CustomSerializer(source='child')
-    class Meta:
-        model = ShoppingItemBase
-        fields = ('item_type',)
+#class ShoppingItemBaseSerializer(serializers.ModelSerializer):
+#    item_type = CustomSerializer(source='child')
+#    class Meta:
+#        model = ShoppingItemBase
+#        fields = ('item_type',)
 
 class ShoppingItemSerializer(serializers.ModelSerializer):
-    base_item = ShoppingItemBaseSerializer(source='details')
+    data = CustomSerializer(source='details.child')
+
     class Meta:
         model = ShoppingItem
-        fields = ('rix', 'ritype', 'base_item')
+        fields = ('rix', 'ritype', 'data')
+
+    def to_internal_value(self, data):
+        super(ShoppingItemSerializer, self).to_internal_value(data)
+
+#    def validate_data(self, data):
+#        pass
 
     def to_internal_value(self, data):
         super(ShoppingItemSerializer, self).to_internal_value(data)
